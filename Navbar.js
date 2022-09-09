0cf6cf2471c01a315d34bf9c890c68449b052f65
abhiatry <112927296+abhiatry@users.noreply.github.com>
2022-09-09 10:43:25 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class 
+ extends Component {
+  render() {
+    return (
+      <div style={{display:'flex' , background:'skyblue'}}>
+        <h1 style={{margin:'0 0 .3rem 1rem'}}>MOVIE APP</h1>
+        <h2 style={{margin:'0.5rem 0 0 5rem'}}>Favorite</h2>
+      </div>
+    )
+  }
+}
